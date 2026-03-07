502e7b9818cc482cbc9b21d60f873e7be01f93c9
desiboli <adam.benals@gmail.com>
2026-03-07 13:57:46 +0100

    landingpage ui/copy

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,11 @@
-import { Geist, Geist_Mono, Outfit } from "next/font/google"
+import { Geist_Mono, Outfit, Space_Grotesk } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
 
 const outfit = Outfit({ subsets: ['latin'], variable: '--font-sans' })
-
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], variable: '--font-display' })
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", outfit.variable)}
+      className={cn("antialiased", fontMono.variable, "font-sans", outfit.variable, spaceGrotesk.variable)}
     >
       <body>
         <ThemeProvider>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@workspace/ui/components/button"
 import { AppHeader } from "@/components/app-header"
 
 export default function Page() {
@@ -6,12 +5,10 @@ export default function Page() {
     <main>
       <AppHeader />
       <div className="flex min-h-svh p-6">
-        <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div className="flex max-w-lg justify-center items-center mx-auto min-w-0 flex-col gap-4 text-sm leading-loose">
           <div>
-            <h1 className="font-medium">Project ready!</h1>
-            <p>You may now add components and start building.</p>
-            <p>We&apos;ve already added the button component for you.</p>
-            <Button className="mt-2">Button</Button>
+            <h1 className="font-bold font-display uppercase text-6xl text-center">Build faster with Orkesto</h1>
+            <p className="text-center text-muted-foreground font-mono text-lg">Design, development and AI tools for building modern brands and websites.</p>
           </div>
           <div className="text-muted-foreground font-mono text-xs">
             (Press <kbd>d</kbd> to toggle dark mode)
